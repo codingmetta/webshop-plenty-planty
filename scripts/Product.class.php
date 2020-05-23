354d96edbf8f92ea0393d3d1400b39569d046dc6
codingmetta <t.deckardt@outlook.de>
2020-05-23 12:15:32 +0200

    Copy of Old Repo

---
+++ scripts/Product.class.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * Product Class
+ *
+ * @author Talia Deckardt
+ */
+
+class Product {
+
+	private $id;
+	private $name;
+	private $description;
+	private $price; 
+	private $rating; 
+	private $img_path;
+
+
+	function Product($name, $description, $price, $rating, $img_path){
+
+		$this->name= $name;
+		$this->description= $description;
+		$this->price= $price;
+		$this->rating= $rating;
+		$this->img_path= $img_path;
+	}
+
+	/*
+	function _constructor($name, $description, $price, $rating, $img_path){
+
+		$this->name= $name;
+		$this->description= $description;
+		$this->price= $price;
+		$this->rating= $rating;
+		$this->img_path= $img_path;
+	}*/
+
+	function set_id($id) {
+        $this->id = $id;
+    }
+
+    function get_id() {
+        return $this->id;
+    }
+
+    function set_name($name) {
+        $this->name = $name;
+    }
+
+    function get_name() {
+        return $this->name;
+    }
+
+    function set_description($description) {
+        $this->description = $description;
+    }
+
+    function get_description() {
+        return $this->description;
+    }
+
+    function set_price($price) {
+        $this->price = $price;
+    }
+
+    function get_price() {
+        return $this->price;
+    }
+
+    function set_rating($rating) {
+        $this->rating = $rating;
+    }
+
+    function get_rating() {
+        return $this->rating;
+    }
+
+    function set_imgpath($imgpath) {
+        $this->imgpath = $imgpath;
+    }
+
+    function get_imgpath() {
+        return $this->imgpath;
+    }
+}
+
+
+
+
+?>
