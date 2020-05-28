320b6dd3bae29d9ea835317f56888d04de1d00f1
codingmetta <t.deckardt@outlook.de>
2020-05-28 17:46:05 +0200

    Feature password check was added to scripts/registration-submit.php

--- scripts/registration-submit.php
+++ scripts/registration-submit.php
@@ -19,9 +19,26 @@ $surname  = $_POST['surname'];
 $email = $_POST['email'];
 $un = $_POST['username'];
 $passw = $_POST['password'];
+$repassw = $_POST['password-repeat'];
 $token = hash('ripemd128', "$salt1$passw$salt2");
 
-add_user($conn, $role, $forename, $surname, $email, $un, $token);
+$check_passed = check_password($passw, $repassw);
+
+if ($check_passed){
+    add_user($conn, $role, $forename, $surname, $email, $un, $token);
+} else {
+    echo 'Repeated Password is incorrect. Please try again.';
+}
+
+
+/* Add function checkPassword() to check if input of $passw and $repassw are the same*/
+function check_password($pw, $rpw){
+    if($pw == $rpw){
+        return true;
+    } else {
+        return false;
+    }
+}
 
 
 function add_user($conn, $rl, $fn, $sn, $em, $un, $pw)
