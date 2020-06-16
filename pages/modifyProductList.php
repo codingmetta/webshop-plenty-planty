7d42ccbbf307c163ea45ec605d222de788a9fce5
codingmetta <t.deckardt@outlook.de>
2020-06-16 17:10:33 +0200

    Usability: UI for Admin optimized

--- pages/modifyProductList.php
+++ pages/modifyProductList.php
@@ -29,15 +29,16 @@
     <br> <br>
 
     <div class="container-fluid">
-        <div class="card-columns">
-            <div class="card bg-light d-inline-flex" id="left-card" style="max-width:300px">
-                <div class="card-body">
-                    <form name= "form" action="../scripts/createProduct-submit.php" method="post">
-        
-                        <h3>Create Product</h3>
+        <div class="card-columns d-flex flex-row justify-content-center">
+            <div class="card bg-info" id="left-card" style="max-width:22% ">
+                <div class="card-header">
+                <br>
+                    <h2>Create Product</h2>
                         <br>
                         <p>Please fill out these fields to create a new product.</p>
-                        <hr>
+                </div>
+                <div class="card-body">
+                    <form name= "form" action="../scripts/createProduct-submit.php" method="post">
                         <br>  
                         <label for="productname"><b>Productname    </b></label>
                         <input type="text" placeholder="e.g. Ufo Plant " name="product_name" id="product_name" required>
@@ -45,11 +46,11 @@
                         
                         <label for="price"><b>Price in €    </b></label>
                         <input type="text" placeholder="e.g. 12.95 " name="price" id="price" required> 
-                        <br> 
+                        <br><br> 
 
-                        <label for="amount"><b> Amount in Stock </b></label>
+                        <label for="amount"><b> Amount in Stock   </b></label>
                         <input type="number"  name="amount" id="amount" required>
-                         <br> <hr>
+                         <br> <br><hr>
 
                         <label for="img_path"><b> Relative Image Path </b></label>
                         <input type="text" placeholder="e.g. ../img/pilea.jpg" name="img_path" id="img_path" required>
@@ -61,10 +62,8 @@
                         <p style="text-size: 6px">e.g. pilea peperomioides | beginner-friendly | southern china</p>
                         <textarea type="text" name="description" id="description" rows="4" cols="24" style="" required>
                         </textarea>
-                        <br> <br> <br> 
-
-                        <button type="submit" class="btn btn-primary">Add Product</button>
-                        <br> 
+                         <br> <br><br> <br><hr> <br> 
+                        <button type="submit" class="btn btn-block btn-dark"><i class="fas fa-plus"></i>  Add Product</button>
                     </form>
                 </div>
             </div>
@@ -73,78 +72,92 @@
         
 
 
-        <div class="card card-body bg-light" id="right-card" style="min-width: 1100px">
-            <h1 class="text-center"> Hello, Admin! </h1>
-            <br> <br>
-            <h3> Product List </h3>
-            <br>
-            <table class="table table-striped table-bordered" id="productList">
-                <thead> 
-                    <tr>
-                        <th>Product ID</th>
-                        <th>Productname</th>
-                        <th>Price in €</th>
-                        <th>Amount in Stock</th>
-                    </tr>
-                </thead>
-
-                <?php
-                require '../scripts/login.php';
-
-                $conn = new mysqli($servername, $username, $password, $dbname);
-                if ($conn->connect_error) {
-                    die("Connection failed: " . $conn->connect_error);
-                } 
-
-                $sql = "SELECT product_id, name, price, amount FROM Products";
-                $result = $conn->query($sql);
-
-                ?>
-                <tbody>
-                <?php
-                if ($result->num_rows > 0) {
-                    while($row = mysqli_fetch_array($result)) {
-                        echo "<tr> 
-                        <td>" . $row[0] .  "</td>
-                        <td>" . $row[1] .  "</td>
-                        <td>" . $row[2] .  "</td>
-                        <td>" . $row[3] .  "</td>
-                        </tr>";
-                    }
-                } else {
-                    echo "0 results";
-                }
-                $conn->close();
-                ?>
-                </tbody>
-            </table>
-            <br>
-            <hr>
-
-            <div class="flex-container" style="display:flex">
-                <div id="update-group">
-                    <form name= "alterProductAmount" action="../scripts/alterProductAmount-submit.php" method="post">
-                    <label for="pid">
-                    Update Product Amount in Stock <br><br><input type="text" placeholder="Product ID" name="pid" id="pid" required>
-                    </label> <br>
-                    <input type="number" placeholder="New Amount" name="newamount" id="newamount" required>
-                    <br><br>
-                    <button type="submit" class="btn btn-primary">  Update </button>
-                    </form>
-                </div>
-            <hr>
-            <br>
-                <div id="delete-group">   
-                    <label for="delete"> Delete Product
-                    <br> <br>
-                    <form name= "deleteProduct" action="../scripts/deleteProduct-submit.php" method="post">
-                    <input type="text" placeholder="Product ID" name="delete" id="delete" required></label>
-                    <br> <br> 
-                    <button type="submit" class="btn btn-danger"><i class="fas fa-trash-alt"></i> Delete </button>
-                    </form>
-                </div>
+        <div class="card d-flex bg-light justify-content-center" id="right-card" style="min-width: 1100px;max_width:78%; padding: 4%" >
+            <div class="card-head">
+                <br> <br>
+                <h1 class="text-center"> Welcome back, Admin! </h1>
             </div>
-            <br> 
+            <div class="card-body">
+                <br> 
+                <h3> Product List </h3>
+                <br>
+                <table class="table table-striped table-bordered" id="productList">
+                    <thead> 
+                        <tr>
+                            <th>Product ID</th>
+                            <th>Productname</th>
+                            <th>Price in €</th>
+                            <th>Amount in Stock</th>
+                        </tr>
+                    </thead>
+
+                    <?php
+                    require '../scripts/login.php';
+
+                    $conn = new mysqli($servername, $username, $password, $dbname);
+                    if ($conn->connect_error) {
+                        die("Connection failed: " . $conn->connect_error);
+                    } 
+
+                    $sql = "SELECT product_id, name, price, amount FROM Products";
+                    $result = $conn->query($sql);
+
+                    ?>
+                    <tbody>
+                    <?php
+                    if ($result->num_rows > 0) {
+                        while($row = mysqli_fetch_array($result)) {
+                            echo "<tr> 
+                            <td>" . $row[0] .  "</td>
+                            <td>" . $row[1] .  "</td>
+                            <td>" . $row[2] .  "</td>
+                            <td>" . $row[3] .  "</td>
+                            </tr>";
+                        }
+                    } else {
+                        echo "0 results";
+                    }
+                    $conn->close();
+                    ?>
+                    </tbody>
+                </table>
+                <br>
+                <hr>
+
+                <div class="d-flex flex-row justify-content-between">
+                    <div id="update-group" class="card text-white bg-secondary  p-2" style="max-width: 40%">
+                        <div class="card-header">
+                        <h5> Update Product Amount in Stock </h5>       
+                        </div>
+                        <div class="card-body">
+                            <form name= "alterProductAmount" action="../scripts/alterProductAmount-submit.php" method="post">
+                            <label for="pid">
+                            <input type="text" placeholder="Product ID" name="pid" id="pid" required>
+                            </label> 
+                            <input type="number" placeholder="New Amount" name="newamount" id="newamount" required>
+                            <hr> <br> 
+                            <button type="submit" class="btn btn-block btn-primary"><i class="fas fa-sync-alt"></i>  Update </button>
+                            </form>
+                        </div>
+                    </div>
+                
+                    <div id="delete-group" class="card text-white bg-dark p-2" style="max-width: 25%" >
+                        <div class="card-header">
+                            <h5> Delete Product </h5>
+                        </div>
+                        <div class="card-body">
+                            <label for="delete">
+                      
+                            <form name= "deleteProduct" action="../scripts/deleteProduct-submit.php" method="post">
+                            <input type="text" placeholder="Product ID" name="delete" id="delete" required></label>
+                       
+                        <br> <br> <br> <hr> <br>
+                            <button type="submit" class="btn btn-danger btn-block"><i class="fas fa-trash-alt"></i> Delete </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div> 
         </div>    
     </div>
 </div>
