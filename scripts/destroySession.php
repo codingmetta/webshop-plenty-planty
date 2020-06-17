b8262ea9db94984e8b24c5205534f71d6ca05302
codingmetta <t.deckardt@outlook.de>
2020-06-17 13:51:14 +0200

    Buggy: destroySession.php, logout.php -> authetication not solved yet

---
+++ scripts/destroySession.php
@@ -0,0 +1,36 @@
+<?php
+
+if (isset($_SESSION['username']))
+  {
+    $username = $_SESSION['username'];
+    $password = $_SESSION['password'];
+    $forename = $_SESSION['forename'];
+    $surname  = $_SESSION['surname'];
+
+    //destroy_session_and_data();
+	destroySession();
+    echo "Welcome back $forename.<br>
+          Your full name is $forename $surname.<br>
+          Your username is '$username'
+          and your password is '$password'.";
+  }
+    echo "Destroyed Session.Please <a href='../pages/loginUser.php'>click here</a> to log in.";
+
+
+
+  function destroySession()
+  {
+    $_SESSION=array();
+
+    if (session_id() != "" || isset($_COOKIE[session_name()]))
+      setcookie(session_name(), '', time()-2592000, '/');
+
+    session_destroy();
+  }
+  function destroy_session_and_data()
+  {
+    $_SESSION = array();
+    setcookie(session_name(), '', time() - 2592000, '/');
+    session_destroy();
+  }
+?>
---
+++ scripts/logout.php
@@ -0,0 +1,12 @@
+<?php  
+  require 'destroySession.php';
+
+  if (isset($_SESSION['username']))
+  {
+    destroySession();
+    echo "You have been logged out. Please " .
+         "<a href='index.php'>click here</a> to refresh the screen.";
+  }
+  else echo "You cannot log out because you are not logged in";
+
+?>
