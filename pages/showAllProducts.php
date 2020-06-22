9a01f366e359c0df6d132c676be340bef17eb1e0
codingmetta <t.deckardt@outlook.de>
2020-06-22 11:37:54 +0200

    Started Feature: Price now visible in order

--- pages/showAllProducts.php
+++ pages/showAllProducts.php
@@ -141,6 +141,7 @@ while( $record = mysqli_fetch_assoc($resultset) ) {
         <div class="d-flex justify-content-between" style="display:flex">
             <div class="desc" id="btn-buy">
                     <form name="form" action="../scripts/order-submit.php" method="post">
+                    <input type = "hidden" name ="product_price" value ="<?php echo $record['price']; ?>" />
                     <input type = "hidden" name ="product_name" value ="<?php echo $record['name']; ?>" />
                     <input type = "hidden" name ="product_id" value ="<?php echo $record['product_id']; ?>" />
                     <button type="submit" class="btn btn-primary"><i class="fas fa-shopping-cart"></i> Buy</button>
